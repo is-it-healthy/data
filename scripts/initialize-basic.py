dc3a9b1909fb4d16b14c6ff49909cbbc108161a4
hirusha-adi <hirushaadi@gmail.com>
2025-04-15 19:03:43 +0530

    basic split data

--- scripts/initialize-basic.py
+++ scripts/initialize-basic.py
@@ -3,6 +3,19 @@ This script will initialize everything!
 """
 
 import os
+import json
+import typing as t
 
-with open("dev.js", "r", encoding="utf-8") as _file:
-    _file.read()
+with open(
+    os.path.join("development", "ins-original.json"), "r", encoding="utf-8"
+) as _file:
+    data: t.Dict = json.load(_file)
+
+for key, value in data.items():
+    with open(
+        os.path.join("development", "original-split-single", f"{key}.json"),
+        "w",
+        encoding="utf-8",
+    ) as _file:
+        if value:
+            json.dump(value, _file, indent=4)
