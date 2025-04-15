1f49b3db567b8621c4c81b9f16e38c7673f8665b
hirusha-adi <hirushaadi@gmail.com>
2025-04-15 19:04:49 +0530

    create individual files

--- scripts/initialize-basic.py
+++ scripts/initialize-basic.py
@@ -6,16 +6,10 @@ import os
 import json
 import typing as t
 
-with open(
-    os.path.join("development", "ins-original.json"), "r", encoding="utf-8"
-) as _file:
+with open(os.path.join("development", "ins-original.json"), "r", encoding="utf-8") as _file:
     data: t.Dict = json.load(_file)
 
 for key, value in data.items():
-    with open(
-        os.path.join("development", "original-split-single", f"{key}.json"),
-        "w",
-        encoding="utf-8",
-    ) as _file:
+    with open(os.path.join("development", "original-split-single", f"{key}.json"), "w", encoding="utf-8",) as _file:
         if value:
             json.dump(value, _file, indent=4)
