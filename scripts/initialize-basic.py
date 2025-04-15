7a14e70ad5121a08b1fce630acb8141a880fd0e8
hirusha-adi <hirushaadi@gmail.com>
2025-04-15 19:15:37 +0530

    file name change

--- scripts/initialize-basic.py
+++ scripts/initialize-basic.py
@@ -2,6 +2,7 @@
 This script will initialize everything!
 """
 
+import re
 import os
 import json
 import typing as t
@@ -9,7 +10,22 @@ import typing as t
 with open(os.path.join("development", "ins-original.json"), "r", encoding="utf-8") as _file:
     data: t.Dict = json.load(_file)
 
+# for ./development dir
+# ---------------------
 for key, value in data.items():
-    with open(os.path.join("development", "original-split-single", f"{key}.json"), "w", encoding="utf-8",) as _file:
+    clean_key = re.sub(r"[()]", "", key)
+    filename = f"E{clean_key}.json"
+    with open(os.path.join("development", "original-split-single", filename), "w", encoding="utf-8",) as _file:
         if value:
             json.dump(value, _file, indent=4)
+
+# for ./dist dir
+# --------------
+# for key, value in data.items():
+    # clean_key = re.sub(r"[()]", "", key)
+    # filename = f"E{clean_key}.json"
+    # current_file = os.path.join("dist", "single", filename)
+    # if not os.path.isfile(current_file):
+    #     with open(current_file, "w", encoding="utf-8",) as _file:
+    #         if value:
+    #             json.dump(value, _file, indent=4)
