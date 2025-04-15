374c8d5dc3961c0426969748c799a0ce7c1217f2
hirusha-adi <hirushaadi@gmail.com>
2025-04-15 19:18:29 +0530

    add to dist

--- scripts/initialize-basic.py
+++ scripts/initialize-basic.py
@@ -21,11 +21,11 @@ for key, value in data.items():
 
 # for ./dist dir
 # --------------
-# for key, value in data.items():
-    # clean_key = re.sub(r"[()]", "", key)
-    # filename = f"E{clean_key}.json"
-    # current_file = os.path.join("dist", "single", filename)
-    # if not os.path.isfile(current_file):
-    #     with open(current_file, "w", encoding="utf-8",) as _file:
-    #         if value:
-    #             json.dump(value, _file, indent=4)
+for key, value in data.items():
+    clean_key = re.sub(r"[()]", "", key)
+    filename = f"E{clean_key}.json"
+    current_file = os.path.join("dist", "single", filename)
+    if not os.path.isfile(current_file):
+        with open(current_file, "w", encoding="utf-8",) as _file:
+            if value:
+                json.dump(value, _file, indent=4)
